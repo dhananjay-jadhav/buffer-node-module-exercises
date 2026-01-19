f9bf61d1224be8e782527fa0a57cc7f124dd376b
dhananjay-jadhav <jadhav9260@gmail.com>
2026-01-19 08:11:53 +0530

    Fix

--- exercises/exercise-6-concatenate-multiple-buffers.ts
+++ exercises/exercise-6-concatenate-multiple-buffers.ts
@@ -7,3 +7,23 @@
 // Measure the performance difference when providing totalLength
 
 
+const buffer1 = Buffer.from('Hello');
+const buffer2 = Buffer.from(', ');
+const buffer3 = Buffer.from('World!');
+
+console.time();
+const concatenatedBuffer = Buffer.concat([buffer1, buffer2, buffer3]);
+console.timeEnd();
+
+console.log(concatenatedBuffer);
+console.log(concatenatedBuffer.toString());
+console.log(concatenatedBuffer.length);
+
+
+console.time();
+const concatTrunBuffer = Buffer.concat([buffer1, buffer2, buffer3], 10);
+console.timeEnd();
+
+console.log(concatTrunBuffer);
+console.log(concatTrunBuffer.toString());
+console.log(concatTrunBuffer.length);
